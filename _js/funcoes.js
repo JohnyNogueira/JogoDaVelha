c79d585f3e64db1a8d579654cdd8128a01316498
thalesfsb <thalesfsilveira@hotmail.com>
2018-02-16 16:56:31 -0200

    Criando matriz para comparar os quadros

--- _js/funcoes.js
+++ _js/funcoes.js
@@ -1,8 +1,6 @@
 var cont = 0;
-
 function eventClick(div) {
   cont++;
-  alert(div.id);
   for (var i = cont; i <= 9; i++) {
     if (cont % 2 == 0) {
       div.style.backgroundImage =
@@ -12,47 +10,41 @@ function eventClick(div) {
         "url('https://media1.tenor.com/images/3bc22abcece7e4ed67596760e98c5a4e/tenor.gif')";
     }
   }
-  var quadro1 = document.getElementById("quadro1").style.backgroundImage;
-  var quadro2 = document.getElementById("quadro2").style.backgroundImage;
-  var quadro3 = document.getElementById("quadro3").style.backgroundImage;
-  var quadro4 = document.getElementById("quadro4").style.backgroundImage;
-  var quadro5 = document.getElementById("quadro5").style.backgroundImage;
-  var quadro6 = document.getElementById("quadro6").style.backgroundImage;
-  var quadro7 = document.getElementById("quadro7").style.backgroundImage;
-  var quadro8 = document.getElementById("quadro8").style.backgroundImage;
-  var quadro9 = document.getElementById("quadro9").style.backgroundImage;
-  var result = new Array[(3, 3)]();
-  var quadros = new Array(
-    quadro1,
-    quadro2,
-    quadro3,
-    quadro4,
-    quadro5,
-    quadro6,
-    quadro7,
-    quadro8,
-    quadro9
-  );
+  debugger;
+  var matriz = [
+    [
+      document.getElementById("quadro1").style.backgroundImage,
+      document.getElementById("quadro2").style.backgroundImage,
+      document.getElementById("quadro3").style.backgroundImage
+    ],
+    [
+      document.getElementById("quadro4").style.backgroundImage,
+      document.getElementById("quadro5").style.backgroundImage,
+      document.getElementById("quadro6").style.backgroundImage
+    ],
+    [
+      document.getElementById("quadro7").style.backgroundImage,
+      document.getElementById("quadro8").style.backgroundImage,
+      document.getElementById("quadro9").style.backgroundImage
+    ]
+  ];
 
-  for (var i = 0; i < array(length); i++) {
-    for (var j = 0; i < array(length); j++) {
-      if (array[(i, j)].style.backgroundImage) {
-      }
-    }
+  if (matriz[0][0] == matriz[0][1] && matriz[0][0] == matriz[0][2]) {
+    alert("teste");
   }
-
-  //   if (
-  //     document.getElementById("quadro1").style.backgroundImage ==
-  //       document.getElementById("quadro2").style.backgroundImage &&
-  //     document.getElementById("quadro1").style.backgroundImage ==
-  //       document.getElementById("quadro3").style.backgroundImage
-  //   ) {
-  //     document.write("ganhou");
-  //   } else if (
-  //     document.getElementById("quadro4").style.backgroundImage ==
-  //       document.getElementById("quadro5").style.backgroundImage &&
-  //     document.getElementById("quadro4").style.backgroundImage ==
-  //       document.getElementById("quadro6").style.backgroundImage
-  //   ) {
-  //   }
 }
+
+//   if (
+//     document.getElementById("quadro1").style.backgroundImage ==
+//       document.getElementById("quadro2").style.backgroundImage &&
+//     document.getElementById("quadro1").style.backgroundImage ==
+//       document.getElementById("quadro3").style.backgroundImage
+//   ) {
+//     document.write("ganhou");
+//   } else if (
+//     document.getElementById("quadro4").style.backgroundImage ==
+//       document.getElementById("quadro5").style.backgroundImage &&
+//     document.getElementById("quadro4").style.backgroundImage ==
+//       document.getElementById("quadro6").style.backgroundImage
+//   ) {
+//   }
