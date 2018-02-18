6c03a4d3c2c556ecdc0b94ff52659a0c50954a65
thalesfs <thalesfsilveira.com>
2018-02-18 20:53:50 -0300

    Alterando if que verifica quem ganhou

--- _js/funcoes.js
+++ _js/funcoes.js
@@ -1,15 +1,15 @@
+debugger;
 var cont = 0;
 function eventClick(div) {
   cont++;
-  for (var i = cont; i <= 9; i++) {
-    if (cont % 2 == 0) {
-      div.style.backgroundImage =
-        "url('https://78.media.tumblr.com/37b05aae4ac14a79f35cb23df1703703/tumblr_oazc5z08MX1tszwcio1_500.gif')";
-    } else {
-      div.style.backgroundImage =
-        "url('https://media1.tenor.com/images/3bc22abcece7e4ed67596760e98c5a4e/tenor.gif')";
-    }
+  if (cont % 2 == 0) {
+    div.style.backgroundImage =
+      "url('https://78.media.tumblr.com/37b05aae4ac14a79f35cb23df1703703/tumblr_oazc5z08MX1tszwcio1_500.gif')";
+  } else {
+    div.style.backgroundImage =
+      "url('https://media1.tenor.com/images/3bc22abcece7e4ed67596760e98c5a4e/tenor.gif')";
   }
+
   debugger;
   var matriz = [
     [
@@ -29,11 +29,28 @@ function eventClick(div) {
     ]
   ];
   debugger;
-  if (matriz[0][0] == matriz[0][1] && matriz[0][0] == matriz[0][2]) {
-    alert("teste");
-  } else if (matriz[1][0] == matriz[1][1] && matriz[1][0] == matriz[1][2]) {
-    alert("teste");
+  for (var L = 0; L < 3; L++) {
+    for (var C = 0; C < 3; C++) {
+      if (
+        matriz[L][C] != "" &&        
+        matriz[L][C] == matriz[L][1] &&
+        matriz[L][C] == matriz[L][2]
+      ) {
+        break;
+      }
+      break;
+      alert("Ganho!");
+    }
   }
+  //  else if (
+  //   matriz[1][0] != "" &&
+  //   matriz[1][1] != "" &&
+  //   matriz[1][2] != "" &&
+  //   matriz[1][0] == matriz[1][1] &&
+  //   matriz[1][1] == matriz[1][2]
+  // ) {
+  //   alert("teste");
+  // }
 }
 
 //   } {
